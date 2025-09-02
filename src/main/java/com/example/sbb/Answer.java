dc746097283387c3488ac4fc682b8eb8200e5cfa
seungmoson <seungmo975@gmail.com>
2025-09-02 15:46:09 +0900

    2-04 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,27 @@
+package com.example.sbb;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
