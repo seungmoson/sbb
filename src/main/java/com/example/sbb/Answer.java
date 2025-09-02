8354a20690b31c90f4c6b9c9bcba4f088f18b5f7
seungmoson <seungmo975@gmail.com>
2025-09-02 16:19:25 +0900

    2-04 엔티티로 테이블 매핑하기-mysql

--- src/main/java/com/example/sbb/Answer.java
+++ src/main/java/com/example/sbb/Answer.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.aspectj.weaver.patterns.TypePatternQuestions;
 
 @Getter
 @Setter
